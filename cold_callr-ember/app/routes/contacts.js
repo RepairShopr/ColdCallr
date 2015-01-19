0c59d4fe1c0b7f22aed0078dc7987d2fe599b526
Blair Anderson <Blair81@gmail.com>
2015-01-19 07:49:18 -0800

    add redirect to login on 401

--- cold_callr-ember/app/routes/contacts.js
+++ cold_callr-ember/app/routes/contacts.js
@@ -16,6 +16,14 @@ export default Ember.Route.extend(PaginationRouteMixin,{
 
   model: function(params) {
     return this.store.find('contact', {page: params.page, sort_by: params.sortBy, status: params.status, query: params.query});
-  }
+  },
 
+  //TODO: refactor to use https://github.com/simplabs/ember-simple-auth#readme
+    actions: {
+        error: function (error, transition) {
+            if (error && error.status === 401 && error.statusText === "Unauthorized") {
+                window.location.href = window.location.origin + "/users/sign_in"
+            }
+        }
+    }
 });
