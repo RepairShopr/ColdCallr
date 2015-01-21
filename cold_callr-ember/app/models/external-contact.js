b1a6817bb3738bcfb4b0b7afa0e6cefb7d1233a1
Blair Anderson <Blair81@gmail.com>
2015-01-21 12:25:42 -0800

    implement properties transform

--- cold_callr-ember/app/models/external-contact.js
+++ cold_callr-ember/app/models/external-contact.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   contact: DS.belongsTo("contact"),
 
-  name: DS.attr(),
+  name: DS.attr('string'),
   phone: DS.attr(),
-  properties: DS.attr()
+  properties: DS.attr('properties')
 });
